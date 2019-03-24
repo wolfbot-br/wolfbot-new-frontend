271462cea17d48fb18748fcac7f08eff7b273770
Vinícius Rocha <vnc.chelsea@gmail.com>
2019-03-24 10:28:05 -0300

    feat(account/login): acessando local storage para controlar rotas

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import promise from 'redux-promise'
-import { Provider } from 'react-redux'
-import reducers from './reducers'
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import promise from "redux-promise";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import functions from "./utils/functions";
 
 import AuthLayout from "layouts/Auth/Auth.jsx";
 import AdminLayout from "layouts/Admin/Admin.jsx";
@@ -16,20 +17,39 @@ import "assets/scss/black-dashboard-pro-react.scss?v=1.0.0";
 import "assets/demo/demo.css";
 import "react-notification-alert/dist/animate.css";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise, thunk)(createStore)(reducers, devTools)
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise, thunk)(createStore)(reducers, devTools);
 const hist = createBrowserHistory();
+
+const authenticate = props => {
+  const userLocalStorage = functions.loadLocalStorage("user_bot");
+  const { path } = props.match;
+
+  if (!userLocalStorage && path !== "/auth")
+    return <Redirect from={props.location.pathname} to="/auth/login" />;
+
+  if (path === "/auth") functions.removeLocalStorageItem("user_bot");
+
+  switch (path) {
+    case "/auth":
+      return <AuthLayout {...props} />;
+    case "/admin":
+      return <AdminLayout {...props} />;
+    default:
+      return <AuthLayout {...props} />;
+  }
+};
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hist}>
       <Switch>
-        <Route path="/auth" render={props => <AuthLayout {...props} />} />
-        <Route path="/admin" render={props => <AdminLayout {...props} />} />
+        <Route path="/auth" render={props => authenticate(props)} />
+        <Route path="/admin" render={props => authenticate(props)} />
         <Redirect from="/" to="/auth/login" />
       </Switch>
     </Router>
-  </Provider>
-  , document.getElementById("root")
+  </Provider>,
+  document.getElementById("root")
 );
--- src/views/Backtest/BacktestAction.js
+++ src/views/Backtest/BacktestAction.js
@@ -1,18 +1,24 @@
+<<<<<<< HEAD
 import axios from 'axios'
 // import { toastr } from 'react-redux-toastr'
 import ambiente from '../config-ambiente'
 import functions from '../helpers/functions'
+=======
+import axios from "axios";
+import { toastr } from "react-redux-toastr";
+import ambiente from "../config-ambiente";
+import functions from "../helpers/functions";
+>>>>>>> feat(account/login): acessando local storage para controlar rotas
 
-const USER_BOT = functions.loadLocalStorage('user_bot')
+const USER_BOT = functions.loadLocalStorage("user_bot");
 
 export function testarStrategy(values) {
   const url = `${ambiente.URL.api}/backtest/testarConfiguracao`
   return dispatch => {
-    axios.post(url, values, { headers: { authorization: USER_BOT.Token } })
+    axios
+      .post(url, values, { headers: { authorization: USER_BOT.Token } })
       .then(resp =>
-        dispatch(
-          { type: 'RESULT_FETCHED', payload: resp.data.data }
-        )
+        dispatch({ type: "RESULT_FETCHED", payload: resp.data.data })
       )
       .catch(e => {
         // for (var i = 0; i < e.response.data.errors.length; i++) {
